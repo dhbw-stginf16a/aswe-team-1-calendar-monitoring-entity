86251b8f954ac86c54bf53ea57d7a22b4705c785
Thore <thore@kruess.xyz>
2019-03-10 17:17:31 +0100

    Copy structure from central router

---
+++ app.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import logging
+
+import connexion
+from flask_cors import CORS
+
+
+logger = logging.getLogger(__name__)
+
+
+app = connexion.App(__name__, specification_dir='openapi/')
+app.add_api('openapi.yml')
+
+# Set CORS headers
+CORS(app.app)
+
+# set the WSGI application callable to allow using uWSGI:
+# uwsgi --http :8080 -w app
+application = app.app
+
+logger.info('App initialized')
+
+if __name__ == '__main__':
+    # run our standalone server
+    app.run(port=8080)
