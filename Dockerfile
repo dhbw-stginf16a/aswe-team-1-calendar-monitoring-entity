8a32a67f515133852dd2935b6daee3b95e2a5de6
Dorian Czichotzki <dorian.czichotzki@de.ibm.com>
2019-03-20 12:36:42 +0100

    Deployment Fix

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,7 @@ RUN adduser -D flask && pip install pipenv
 
 WORKDIR /app
 
-COPY . /app
+ADD deploy.tar /app/
 
 RUN pipenv install --deploy --system && chown -R flask:flask /app
 
--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+tar -cf deploy.tar Pipfile Pipfile.lock app.py openapi api
+
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker build -t asweteam1/calendar-monitor:latest -t asweteam1/calendar-monitor:$TRAVIS_TAG --label version="$TRAVIS_TAG" .
 docker push asweteam1/calendar-monitor:latest
